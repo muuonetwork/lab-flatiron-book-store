e9ff5b82b2a6c0ee2e3673b48b833dd05662d809
gideon musee <muuonetwork@gmail.com>
2026-04-19 16:33:59 +0300

    Add async fetch and displayPosts functionality

--- index.js
+++ index.js
@@ -1,65 +1,25 @@
-const bookStore = {
-    name: 'Flatbooks Technical Books',
-    books: [
-        {
-            id:1,
-            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-            author: 'Marjin Haverbeke',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-        },
-        {
-            id:2,
-            title: 'JavaScript & JQuery: Interactive Front-End Web Development',
-            author: 'Jon Duckett',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
-        },
-        {
-            id:3,
-            title: 'JavaScript: The Good Parts',
-            author: 'Douglas Crockford',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-        },
-        {
-            id:4,
-            title: 'JavaScript: The Definitive Guide',
-            author: 'David Flanagan',
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-        },
-        {
-            id:5,
-            title: "You Don't Know JS",
-            author: 'Kyle Simpson',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
-        },
-        {
-            id:6,
-            title: 'Cracking the Coding Interview',
-            author: 'Gayle Laakmann McDowell',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-        }
-    ]
+function displayPosts(posts) {
+  const ul = document.getElementById("post-list");
+
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+
+    const p = document.createElement("p");
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
+  });
 }
 
-// Write your code here!
-const bookStoreTitle = document.querySelector('#header')
-document.querySelector('#delete-this').remove()
-bookStoreTitle.textContent = bookStore.name
-
-function addToBookList(book) {
-    const bookContainer = document.createElement('li')
-    const bookTitle = document.createElement('h3')
-    const bookAuthor = document.createElement('p')
-    const bookImage = document.createElement('img')
-
-    bookTitle.textContent = book.title
-    bookAuthor.textContent = book.author
-    bookImage.src = book.imageUrl
-
-    const bookList = document.querySelector('#book-list')
-    bookContainer.append(bookTitle, bookAuthor, bookImage)
-    bookList.append(bookContainer)
+async function fetchPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  displayPosts(posts);
 }
 
-bookStore.books.forEach(book => {
-    addToBookList(book)
-})
+fetchPosts();
